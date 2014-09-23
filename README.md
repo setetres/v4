3c04bc90f9684b87549b73afa50c5cf10a27cda4
setetres <setetres@gmail.com>
2014-09-23 02:34:41 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+v4
+==
+
+v4.0 of my website.
+
+![v4](http://file.setetres.st/img/v4-header.gif?v=1&raw=true)
+
+License
+-------
+
+v4 is under a [Creative Commons Attribution License].
+
+[setetres.st]: http://setetres.st
+[Creative Commons Attribution License]: http://creativecommons.org/licenses/by/4.0
---
+++ js/functions.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+
+    // tumblr
+
+    $.ajax({
+        type: "GET",
+        //url: "http://api.tumblr.com/v2/tagged?tag=gif",
+        url: "http://api.tumblr.com/v2/blog/stream.setetres.st/posts/photo?offset=" + Math.floor(Math.random() * 501),
+        dataType: "jsonp",
+        data: {
+            api_key: "TOkL9fw31Ighuo3zysLk5j1ZrksrJclrViAfXJReJdMrt2GYvf"
+        }
+    }).done(function(data){
+        var url = $.rand(data.response.posts);
+        var alt = url.photos[0].alt_sizes[0];
+        url = alt.width == 1280 ? alt.url : url.photos[0].original_size.url;
+        console.log(url);
+        console.log($.rand(data.response.posts));
+        $('.wr.white').css('background-image', 'url(' + url + ')');
+        $('.wr.white').css('background-size', 'cover');
+    });
+
+    // window load
+
+    $(window).load(function(){
+    });
+
+    // window resize
+
+    $(window).resize(function(){
+        $('#iframe').css('height',$(window).height()-108);
+    });
+
+    // screenfull
+
+    $('#toggle').click(function(){
+        screenfull.toggle($('#container')[0]);
+        return false;
+    });
+
+    // iframe
+
+    $('#iframe').css('height',$(window).height()-108);
+
+    $('.logo a').click(function(){
+        $('.browserTabs li').removeClass('active');
+        $('.browserTabs li:nth-child(1)').addClass('active');
+    });
+
+    $('.browserTabs li a').click(function(){
+        $('.browserTabs li').removeClass('active');
+        $('.browserTabs li.right').addClass('active');
+        $(this).parent().addClass('active');
+    });
+
+    // menu
+
+    $('.browserToolbar .menu').click(function(e){
+        $('.popup, .tail').toggleClass('active');
+         e.stopPropagation();
+    });
+
+    $('.popup, .tail').click(function(e){
+        e.stopPropagation();
+    });
+
+    $('html, body, .iframe').click(function(){
+        $('.popup, .tail').removeClass('active');
+    });
+
+    // targets
+
+    $('[rel=external]').attr('target','_blank');
+    $('[rel=canvas]').attr('target','canvasBrowser');
+
+});
+
+(function ($){
+    $.rand = function (arg){
+        if ($.isArray(arg)){
+            return arg[$.rand(arg.length)];
+        } else if (typeof arg == "number"){
+            return Math.floor(Math.random() * arg);
+        }
+    };
+})(jQuery);
